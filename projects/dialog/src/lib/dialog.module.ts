6a8c9233b3e5508a0fc2b138e370b101ab10e834
Danny Ng <dannyboyng@gmail.com>
2019-10-29 14:06:48 +0100

    Add support for dragging dialog window

--- projects/dialog/src/lib/dialog.module.ts
+++ projects/dialog/src/lib/dialog.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { DialogComponent } from './dialog.component';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [DialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DragDropModule,
   ],
   exports: [DialogComponent],
   entryComponents: [DialogComponent]
